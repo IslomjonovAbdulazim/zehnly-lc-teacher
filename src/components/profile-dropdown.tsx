f736442555d142fd73dc2c644ea26eb78e0c36c9
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-09-02 12:34:49 +0500

    imp

--- src/components/profile-dropdown.tsx
+++ src/components/profile-dropdown.tsx
@@ -9,7 +9,6 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { SignOutDialog } from '@/components/sign-out-dialog'
